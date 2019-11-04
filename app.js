be4a932d35835f99ca2e70d8ac3cbce570ea5099
jlopidas <tuffluck256@gmail.com>
2019-11-04 15:38:57 -0500

    weather

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express();
+
+// setting view engine
+app.set('view engine', 'ejs');
+
+// middleware
+app.use(express.static('./public'));
+
+/*
+
+    ROUTES
+*/
+
+//GET/
+app.get('/', function(req,res){
+    res.render('home.ejs')
+});
+
+app.listen(3000, function(){
+    console.log('server is live on port: 3000')
+});
